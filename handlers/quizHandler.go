ab2b55b0afe9a3fe26499f035cd626e42fc18076
Emre Erkmen <emre.erkmen@hotmail.com>
2022-12-04 21:58:44 +0300

    Add quiz handler

--- handlers/quizHandler.go
+++ handlers/quizHandler.go
@@ -2,18 +2,19 @@ package handlers
 
 import (
 	"quiz-app/quiz-api/data"
+	"quiz-app/quiz-api/model"
 
 	"github.com/hashicorp/go-hclog"
 )
 
 // Quiz handler for geting and anwering Quizzes
 type QuizHandler struct {
-	logger         hclog.Logger
-	validation     *data.Validation
-	quizzesDB      *data.QuizzesDB
+	logger        hclog.Logger
+	validation    *data.Validation
+	quizzesModels *model.QuizzesModels
 }
 
 // NewQuizHandler returns a new quiz handler with the given logger
-func NewQuizHandler(logger hclog.Logger, validation *data.Validation, quizDB *data.QuizzesDB) *QuizHandler {
-	return &QuizHandler{logger, validation, quizDB}
+func NewQuizHandler(logger hclog.Logger, validation *data.Validation, quizzesModels *model.QuizzesModels) *QuizHandler {
+	return &QuizHandler{logger, validation, quizzesModels}
 }
