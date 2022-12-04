3c0a26ee64d50bb52059c069193f9ab91dca9494
Emre Erkmen <emre.erkmen@hotmail.com>
2022-12-04 23:23:09 +0300

    Add getting quizzes

--- handlers/getQuiz.go
+++ handlers/getQuiz.go
@@ -1,22 +1,20 @@
 package handlers
 
-import "net/http"
+import (
+	"net/http"
+	"quiz-app/quiz-api/data"
+)
 
-// GetAll handles GET requests and returns all current quizzes
-func (quizHandler *QuizHandler) GetAll(rw http.ResponseWriter, r *http.Request) {
+// GetAllQuizzes handles GET requests and returns all current quizzes
+func (quizHandler *QuizHandler) GetAllQuizzes(rw http.ResponseWriter, r *http.Request) {
 	quizHandler.logger.Debug("Get all records")
 	rw.Header().Add("Content-Type", "application/json")
 
-	prods, err := quizHandler.quizzesDB
-	if err != nil {
-		rw.WriteHeader(http.StatusInternalServerError)
-		data.ToJSON(&GenericError{Message: err.Error()}, rw)
-		return
-	}
+	quizzes := quizHandler.quizzesModels.GetAllQuizzes()
 
-	err = data.ToJSON(prods, rw)
+	err := data.ToJSON(quizzes, rw)
 	if err != nil {
 		// we should never be here but log the error just incase
-		p.l.Error("Unable to serializing product", "error", err)
+		quizHandler.logger.Error("Unable to serializing quizzes", "error", err)
 	}
 }
