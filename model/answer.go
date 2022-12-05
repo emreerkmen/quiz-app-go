fac9283292b4a8aa67068e9217fd633047d5c0a3
Emre Erkmen <emre.erkmen@hotmail.com>
2022-12-06 01:02:33 +0300

    Fix creating a quiz that has no answer when take error

--- model/answer.go
+++ model/answer.go
@@ -7,10 +7,10 @@ import (
 	"github.com/hashicorp/go-hclog"
 )
 
-//1, 1, []int{1, 1, 0}
+// 1, 1, []int{1, 1, 0}
 type Answer struct {
-	QuizID int
-	UserID int
+	QuizID          int
+	UserID          int
 	SelectedChoices []int
 }
 
@@ -18,28 +18,33 @@ type AnswerModels struct {
 	logger hclog.Logger
 }
 
+type answerData struct {
+	question       *data.Question
+	selectedChoice int
+}
+
 func NewAnswerModels(logger hclog.Logger) *AnswerModels {
 	answerModels := AnswerModels{logger: logger}
 	return &answerModels
 }
 
-func(answerModel AnswerModels) AnswerQuiz(answer *Answer) (int, error) {
+func (answerModel AnswerModels) AnswerQuiz(answer *Answer) (int, error) {
 	quiz, err := data.GetQuizByID(answer.QuizID)
 
 	if err != nil {
-		return 0,&ErrorGeneric{err}
+		return 0, &ErrorGeneric{err}
 	}
 
 	_, err = data.GetUser(answer.UserID)
 
 	if err != nil {
-		return 0,&ErrorGeneric{err}
+		return 0, &ErrorGeneric{err}
 	}
 
 	questions, err := data.GetQuestionByQuizID(quiz.ID)
 
 	if err != nil {
-		return 0,&ErrorGeneric{err}
+		return 0, &ErrorGeneric{err}
 	}
 
 	answerLength := len(answer.SelectedChoices)
@@ -49,20 +54,39 @@ func(answerModel AnswerModels) AnswerQuiz(answer *Answer) (int, error) {
 		return 0, &ErrorAnswering{questionLength, answerLength}
 	}
 
-	quizResult := data.CreateNewQuizResult(answer.QuizID, answer.UserID)
-
+	answersDatas := []answerData{}
 	for index, selectedChoice := range answer.SelectedChoices {
+
 		question := (*questions)[index]
 		//choices length kontrolü yap
 		choices, err := data.GetQuestionChoices(question.ID)
 		if err != nil {
-			return 0,&ErrorGeneric{err}
+			return 0, &ErrorGeneric{err}
 		}
+
 		choicesLength := len(*choices)
+
+		//Todo: validation ile bunu yap ve sil
+		if selectedChoice < -1 {
+			return 0, &ErrorUnExpectedChoice{questionID: question.ID, choiceLenght: choicesLength, selectedChoice: selectedChoice-1}
+		}
+
 		if selectedChoice+1 > choicesLength {
 			return 0, &ErrorUnExpectedChoice{questionID: question.ID, choiceLenght: choicesLength, selectedChoice: selectedChoice}
 		}
-		data.CreateNewAnswer(quizResult.ID, question, selectedChoice)
+
+		answerData := answerData{
+			question: question,
+			selectedChoice: selectedChoice,
+		}
+
+		answersDatas=append(answersDatas, answerData)
+	}
+
+	quizResult := data.CreateNewQuizResult(answer.QuizID, answer.UserID)
+
+	for _,answerData := range answersDatas{
+		data.CreateNewAnswer(quizResult.ID, answerData.question, answerData.selectedChoice)
 	}
 
 	return quizResult.ID, nil
@@ -86,3 +110,4 @@ type ErrorUnExpectedChoice struct {
 func (err *ErrorUnExpectedChoice) Error() string {
 	return fmt.Sprintf("Selected a choise that does not eixst. Question ID: %v, Choice Length: %v, Selected Choice: %v", err.questionID, err.choiceLenght, err.selectedChoice+1)
 }
+
